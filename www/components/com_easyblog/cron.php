350aa332c98fcb8f406707cfdf3b080f3046da0d
Connecting Connections <info@con-con.nl>
2020-06-24 16:55:42 +0200

    CronJob New and improved

--- www/components/com_easyblog/cron.php
+++ www/components/com_easyblog/cron.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * @package		EasyBlog
-* @copyright	Copyright (C) 2016 Stack Ideas Private Limited. All rights reserved.
+* @copyright	Copyright (C) Stack Ideas Sdn Bhd. All rights reserved.
 * @license		GNU/GPL, see LICENSE.php
 * EasyBlog is free software. This version may have been modified pursuant
 * to the GNU General Public License, and as distributed it includes or
@@ -10,68 +10,59 @@
 * See COPYRIGHT.php for copyright notices and details.
 */
 
-######################################## 
+########################################
 ##### Configuration options.
 ########################################
 
-// This should not contain http:// or https://
-$host = 'magazine.joomla.org';
+// Please enter the url with the prefix https://yourdomain.com
+$host = 'https://magazine.joomla.org';
+
+// If you have a passphrase set in the configuration of your cronjob, please set it here.
+$phrase = '';
 
 ########################################
 
 // In case the host name is not configured.
-if ($host == 'site.com') {
+if ($host == 'https://site.com') {
 	echo "Please change the \$host value in the cron.php file to your correct url";
 	return;
 }
 
-function connect($host, $url)
+function connect($host, $phrase, $task)
 {
-	// check whether the curl function is exist or not.
-	if (function_exists('curl_version')) {
-		connectCurl($host, $url);
+	if (!function_exists('curl_version')) {
+		echo "cURL needs to be enabled with PHP. Please get in touch with your hosting provider.";
 		return;
 	}
 
-	// default
-	connectFwrite($host, $url);
-	return;
-}
+	$url = 'index.php?option=com_easyblog&task=' . $task;
 
-function connectCurl($host, $url)
-{
-	$ch = curl_init(); 
-    curl_setopt($ch, CURLOPT_URL, "//" . $host . '/' . $url); 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: close'));
-    curl_setopt($ch, CURLOPT_TIMEOUT, 2); 
-    $response = curl_exec($ch); 
-    
-    curl_close($ch);
-}
-
-
-function connectFwrite($host, $url)
-{
-	$fp = @fsockopen($host, 80, $errorNum, $errorStr);
-
-	if (!$fp) {
-		echo 'There was an error connecting to the site.';
-		exit;
+	if ($phrase) {
+		$url .= '&phrase=' . $phrase;
 	}
 
-	$request = "GET /" . $url . " HTTP/1.1\r\n";
-	$request .= "Host: " . $host . "\r\n";
-	$request .= "Connection: Close\r\n\r\n";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $host . '/' . $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: close'));
+	curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+	$response = curl_exec($ch);
 
-	fwrite($fp, $request);
+	curl_close($ch);
 
-	fclose($fp);
+	return $response;
 }
 
-connect($host, 'index.php?option=com_easyblog&task=cron');
-connect($host, 'index.php?option=com_easyblog&task=cronfeed');
+$result = array();
 
-echo "Cronjob processed.\r\n";
-return;
+$response = connect($host, $phrase, 'cron');
+$result[] = json_decode($response);
+
+$response = connect($host, $phrase, 'cronfeed');
+$result[] = json_decode($response);
+
+header('Content-type: text/x-json; UTF-8');
+
+echo json_encode($result);
+exit;
